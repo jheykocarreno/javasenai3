88ceb316830b38aed4258d81bb230929d88b05a3
jheykocarreno <jheyko1000@gmail.com>
2025-05-27 15:44:54 -0300

    ex 5

--- Ex_Introdutorios/src/ex_introdutorios/Ex_Introdutorios.java
+++ Ex_Introdutorios/src/ex_introdutorios/Ex_Introdutorios.java
@@ -36,25 +36,37 @@ public class Ex_Introdutorios {
 //        System.out.println("A soma dos numeros é: " + ((n1 + n2)/2));
         
         //4. Declare variáveis dos tipos int, double, boolean e char. Atribua valores e imprima todos.
-        int varInt;
-        double varDouble;
-        boolean varBoolean;
-        char varChar;
-        
+//        int varInt;
+//        double varDouble;
+//        boolean varBoolean;
+//        char varChar;
+//        
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Ingresse um numero");
+//        varInt = sc.nextInt();
+//        System.out.println("Ingresse um tipo double");
+//        varDouble = sc.nextDouble();
+//        System.out.println("Ingresse um Boolean: True ou False");
+//        varBoolean = sc.nextBoolean();
+//        System.out.println("Ingresse um char/caracter");
+//        varChar = sc.next().charAt(0);
+//        
+//        System.out.println(varInt);
+//        System.out.println(varDouble);
+//        System.out.println(varBoolean);
+//        System.out.println(varChar);
+
+        //5. DESAFIO: Leia o nome, idade e altura de uma pessoa e exiba no formato:
+        //"Nome: Ana | Idade: 20 anos | Altura: 1.65 m
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresse um numero");
-        varInt = sc.nextInt();
-        System.out.println("Ingresse um tipo double");
-        varDouble = sc.nextDouble();
-        System.out.println("Ingresse um Boolean: True ou False");
-        varBoolean = sc.next().charAt(0);
-        System.out.println("Ingresse um char/caracter");
-        varChar = (char) sc.nextShort();
+
+        System.out.println("Nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Idade: ");
+        int idade = sc.nextInt();
+        System.out.println("Altura: ");
+        double altura = sc.nextDouble();
         
-        System.out.println(varInt);
-        System.out.println(varDouble);
-        System.out.println(varBoolean);
-        System.out.println(varChar);
+        System.out.println("Nome: " + nome + " | Idade: " + idade + " | Altura: " + altura);
     }
-    
 }
